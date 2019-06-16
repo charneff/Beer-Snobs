8308d2e7463c94a7c3b844e7f72fdc42f41ceeeb
charneff <charlotte.neff@gmail.com>
2019-06-16 18:26:54 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/auth/:provider/callback' => 'sessions#create'
   resources :reviews
-  resources :beers
+  resources :beers do
+    resources :reviews, only: [:new, :index]
+  end
   resources :breweries
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
