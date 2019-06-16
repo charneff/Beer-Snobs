12f17c66583beaaa4a6329e2f632487d72829185
charneff <charlotte.neff@gmail.com>
2019-06-16 18:32:41 -0500

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :reviewed_beers, through: :reviews, source: :beer
+  has_many :beers #that they created
+
+  validates :username, uniqueness: true, presence: true
+  has_secure_password
+
+  def self.create_by_google_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.password = 'SecureRandom.hex'
+    end
+  end
 end
